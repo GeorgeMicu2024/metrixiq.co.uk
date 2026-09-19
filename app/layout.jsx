8db8a16a33882c84ae7d1f9eb27d131a87be3b37
George Micu <160964514+GeorgeMicu2024@users.noreply.github.com>
2026-09-19 21:19:41 +0100

    v5: load Intelligence & Reporting styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import "./mentor.css";
 import "./governance-v2.css";
 import "./manager-intelligence-v3.css";
 import "./operations-intelligence-v4.css";
+import "./intelligence-reporting-v5.css";
 
 export const metadata = {
   metadataBase: new URL("https://www.metrixiq.co.uk"),
