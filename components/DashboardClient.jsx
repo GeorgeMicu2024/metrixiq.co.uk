aa1fe79a4779952e379b45b1a45286c01519ce34
George Micu <160964514+GeorgeMicu2024@users.noreply.github.com>
2026-09-21 20:35:21 +0100

    feat(cc): route customer compliance to dedicated action centre

--- components/DashboardClient.jsx
+++ components/DashboardClient.jsx
@@ -19,6 +19,7 @@ import { DriverScorecardsView, SiteScorecardsView } from "./scorecards/Scorecard
 import PerformanceView from "./performance/PerformanceView";
 import DriverDirectoryView from "./drivers/DriverDirectoryView";
 import IadcView from "./operations/IadcView";
+import CustomerComplianceView from "./operations/CustomerComplianceView";
 import MentorView from "./operations/MentorView";
 import ConcessionsView from "./operations/ConcessionsView";
 import CoachingV3 from "./coaching/CoachingV3";
@@ -375,7 +376,7 @@ export default function DashboardClient() {
     case "iadc": view = <IadcView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} onImport={() => navigate("imports")} siteFilter={siteFilter} metric="iadc" refreshKey={operationalRefreshKey} />; break;
     case "pod": view = <IadcView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} onImport={() => navigate("imports")} siteFilter={siteFilter} metric="pod" refreshKey={operationalRefreshKey} />; break;
     case "dcr": view = <IadcView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} onImport={() => navigate("imports")} siteFilter={siteFilter} metric="dcr" refreshKey={operationalRefreshKey} />; break;
-    case "cc": view = <IadcView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} onImport={() => navigate("imports")} siteFilter={siteFilter} metric="cc" refreshKey={operationalRefreshKey} />; break;
+    case "cc": view = <CustomerComplianceView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} onImport={() => navigate("imports")} siteFilter={siteFilter} refreshKey={operationalRefreshKey} />; break;
     case "cdf": view = <CdfView organizationId={workspace?.organization?.id} onImport={() => navigate("imports")} siteFilter={siteFilter} />; break;
     case "mentor": view = <MentorView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} onImport={() => navigate("imports")} siteFilter={siteFilter} onSiteFilterChange={setSiteFilter} refreshKey={operationalRefreshKey} />; break;
     case "concessions": view = <ConcessionsView organizationId={workspace?.organization?.id} onOpenDriver={openDriver} siteFilter={siteFilter} />; break;
